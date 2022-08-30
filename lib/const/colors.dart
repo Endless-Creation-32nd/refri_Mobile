34d2794051604cfcf639ec5c424c5e2391571509
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-08-30 23:55:30 +0900

    Feat: Bottom Navigation 디자인 #3

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const PRIMARY_COLOR = Color(0xFFF1F1E7);
 const SECONDARY_COLOR = Color(0xFF9A8266);
 const BLACK_COLOR = Color(0xFF242325);
+const SUB_COLOR = Color(0xFF8D8D8D);
 const TINT_COLOR = Color(0xFFFD7109);
